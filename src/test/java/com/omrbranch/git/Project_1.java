edfeefdbf307c6ba1fdd86cc15af75635da7df92
vasanth27agm <vasanth27agm@gmail.com>
2025-10-28 15:45:54 +0530

    Project Status

---
+++ src/test/java/com/omrbranch/git/Project_1.java
@@ -0,0 +1,19 @@
+package com.omrbranch.git;
+
+public class Project_1 {
+	
+	public void firstCountry( ) {
+		System.out.println("USA");
+			
+	}
+	
+	public void secondCountry() {
+		System.out.println("UK");
+	}
+	
+	public void thirdCountry() {
+		System.out.println("INDIA");
+	}
+	
+
+}
